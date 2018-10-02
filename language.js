2f51e1f2d4a3838a0922d12da755d38f9cb97497
Merryn <merryndd@gmail.com>
2018-10-02 21:12:54 +0100

    Update language.js

--- language.js
+++ language.js
@@ -6,8 +6,8 @@ function initiate() {
     var style1 = document.getElementById("styleen");
     var style2 = document.getElementById("stylekw");
 
-  style1.onclick = function () { swapStyleSheet("css/english.css") };
-  style2.onclick = function () { swapStyleSheet("css/cornish.css") };
+  style1.onclick = function () { swapStyleSheet("/css/english.css") };
+  style2.onclick = function () { swapStyleSheet("/css/cornish.css") };
 }
 
 window.onload = initiate;
