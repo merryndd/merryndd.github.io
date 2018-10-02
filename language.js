03f80248ae5898e996e20ffcb6f6d17ef17c9fa5
Merryn <merryndd@gmail.com>
2018-10-02 21:05:36 +0100

    Create language.js

---
+++ language.js
@@ -0,0 +1,13 @@
+function swapStyleSheet(sheet) {
+    document.getElementById("pagestyle").setAttribute("href", sheet);  
+}
+
+function initiate() {
+    var style1 = document.getElementById("styleen");
+    var style2 = document.getElementById("stylekw");
+
+  style1.onclick = function () { swapStyleSheet("css/english.css") };
+  style2.onclick = function () { swapStyleSheet("css/cornish.css") };
+}
+
+window.onload = initiate;
